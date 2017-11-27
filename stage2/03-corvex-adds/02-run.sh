2c3ee1977b693b2fb6d1b613d9e11f81e5930e85
Shane McCarron <halindrome@gmail.com>
2017-11-27 14:09:11 -0600

    Fixed a typo

--- stage2/03-corvex-adds/02-run.sh
+++ stage2/03-corvex-adds/02-run.sh
@@ -7,5 +7,6 @@ EOF
 install -m 644 files/dnsmasq.conf ${ROOTFS_DIR}/etc
 install -m 644 files/hostapd.conf ${ROOTFS_DIR}/etc/hostapd
 install -m 644 files/000-default.conf ${ROOTFS_DIR}/etc/apache2/sites-enabled
-cp -r ${GW_SCRIPTS} /var/www/html/scripts
+mkdir -p /var/www/html/scripts
+cp -r ${GW_SCRIPTS}/* /var/www/html/scripts
 
