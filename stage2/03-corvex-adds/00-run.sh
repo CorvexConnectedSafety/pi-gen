d514eff14eb5c433d5c4e0236fa4d7b35c28c685
Shane McCarron <halindrome@gmail.com>
2017-11-29 13:58:08 -0600

    More improvements to base configuration.

--- stage2/03-corvex-adds/00-run.sh
+++ stage2/03-corvex-adds/00-run.sh
@@ -1,27 +1,29 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-locale-gen en_US.UTF-8
-update-locale LANG="en_US.UTF-8" LANGUAGE="en_US:en" LC_ALL="en_US.UTF-8"
-if ! id -u corvex >/dev/null 2>&1; then
-    adduser --disabled-password --gecos "" corvex
-fi
-echo "corvex:"$GW_PASS | chpasswd
-adduser corvex sudo
-update-rc.d ssh enable
-EOF
+# enable US
+sed ${ROOTFS_DIR}/etc/locale.gen -i -e "s|# en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|"
 sed ${ROOTFS_DIR}/etc/default/keyboard -i -e "s|gb|us|"
 sed ${ROOTFS_DIR}/etc/avahi/avahi-daemon.conf -i -e "s|workstation=no|workstation=yes|"
 sed ${ROOTFS_DIR}/etc/hosts -i -e "s|raspberrypi|CorvexGW01|"
 sed ${ROOTFS_DIR}/etc/hostname -i -e "s|raspberrypi|CorvexGW01|"
 
-cat >> ${ROOTFS_DIR}/etc/network/interfaces << CONFIG
-auto lo
-iface lo inet loopback
+install -m 755 files/corvex ${ROOTFS_DIR}/etc/init.d/
+install -m 755 files/crontab ${ROOTFS_DIR}/etc/cron.d/corvex
+install -m 440 files/sudo ${ROOTFS_DIR}/etc/sudoers.d/020_corvex-nopasswd
 
-auto eth0
-iface eth0 inet dhcp
-CONFIG
+on_chroot << EOF
+locale-gen en_US.UTF-8
+update-locale en_US.UTF-8
+if ! id -u corvex >/dev/null 2>&1; then
+    adduser --disabled-password --gecos "" corvex
+fi
+echo "corvex:"$GW_PASS | chpasswd
+echo "pi:"$GW_PASS | chpasswd
+adduser corvex sudo
+update-rc.d ssh enable
+update-rc.d corvex defaults
+update-rc.d corvex enable
+EOF
 
 install -m 600 userfiles/tunnel_key ${ROOTFS_DIR}/home/corvex
 install -m 600 userfiles/tunnel_key.pub ${ROOTFS_DIR}/home/corvex
@@ -32,9 +34,11 @@ install -m 755 userfiles/provision.sh ${ROOTFS_DIR}/home/corvex
 install -m 755 userfiles/tunnel.sh ${ROOTFS_DIR}/home/corvex
 
 install -m 644 files/nodesource.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
+install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/interfaces.d/
 
 on_chroot apt-key add - < files/nodesource.gpg.key
 on_chroot << EOF2
 apt-get update
 EOF2
+
 
--- stage2/03-corvex-adds/02-run.sh
+++ stage2/03-corvex-adds/02-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
+install -m 644 files/dnsmasq.conf ${ROOTFS_DIR}/etc
+install -m 644 files/hostapd.conf ${ROOTFS_DIR}/etc/hostapd
+install -m 644 files/000-default.conf ${ROOTFS_DIR}/etc/apache2/sites-enabled
+mkdir -p ${ROOTFS_DIR}/var/www/html/uploads
+mkdir -p ${ROOTFS_DIR}/var/www/html/objects
+mkdir -p ${ROOTFS_DIR}/var/www/html/scripts
+cp -r ${GW_SCRIPTS}/* ${ROOTFS_DIR}/var/www/html/scripts
+
 on_chroot << EOF
 a2enmod rewrite
 a2enmod cgi
 cpanm JSON::DWIW
 EOF
-
-install -m 644 files/dnsmasq.conf ${ROOTFS_DIR}/etc
-install -m 644 files/hostapd.conf ${ROOTFS_DIR}/etc/hostapd
-install -m 644 files/000-default.conf ${ROOTFS_DIR}/etc/apache2/sites-enabled
-mkdir -p ${ROOTFS_DIR}/var/www/html/scripts
-cp -r ${GW_SCRIPTS}/* ${ROOTFS_DIR}/var/www/html/scripts
 
