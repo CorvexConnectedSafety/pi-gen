a917ddf92767eee1b0809f55ab416a9293dc9e36
Shane McCarron <halindrome@gmail.com>
2018-09-24 10:37:42 -0500

    added config params to provision script

--- stage2/03-corvex-adds/userfiles/provision.sh
+++ stage2/03-corvex-adds/userfiles/provision.sh
@@ -10,15 +10,18 @@ configsrc=/home/corvex/config.tmpl
 subID=$1
 locID=$2
 gwName=$3
+beHost=$4
+feHost=$5
+fePath=$6
 
 if [ -z "$subID" -o -z "$locID" ]
 then
-    echo "Usage: provision.sh subscriberID locationID gatewayName"
+    echo "Usage: provision.sh subscriberID locationID gatewayName backendHost frontendHost frontendPath"
     exit 1
 fi
 
-sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/"  -e "s/GATEWAYNAME/$gwName" < $inisrc > $initarget
-sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/" -e "s/GATEWAYNAME/$gwName" < $configsrc > $configtarget
+sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/"  -e "s/GATEWAYNAME/$gwName/" < $inisrc > $initarget
+sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/" -e "s/GATEWAYNAME/$gwName/" -e "s/BACKENDHOST/$beHost/" -e "s/FRONTENDHOST/$feHost/" -e "s/FRONTENDPATH/$fePath/" < $configsrc > $configtarget
 
 chmod +x $configtarget
 
