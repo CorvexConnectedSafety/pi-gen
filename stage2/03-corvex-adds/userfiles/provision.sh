1039c84cf44e5cabe46d812cc22f034380c2a3fa
Shane McCarron <halindrome@gmail.com>
2018-10-03 13:08:52 -0500

    Updated to use configurable port for tunnel.

--- stage2/03-corvex-adds/userfiles/provision.sh
+++ stage2/03-corvex-adds/userfiles/provision.sh
@@ -11,17 +11,18 @@ subID=$1
 locID=$2
 gwName=$3
 beHost=$4
-feHost=$5
-fePath=$6
+bePath=$5
+feHost=$6
+fePath=$7
 
 if [ -z "$subID" -o -z "$locID" ]
 then
-    echo "Usage: provision.sh subscriberID locationID gatewayName backendHost frontendHost frontendPath"
+    echo "Usage: provision.sh subscriberID locationID gatewayName backendHost backendPath frontendHost frontendPath"
     exit 1
 fi
 
 sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/"  -e "s/GATEWAYNAME/$gwName/" < $inisrc > $initarget
-sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/" -e "s/GATEWAYNAME/$gwName/" -e "s/BACKENDHOST/$beHost/" -e "s/FRONTENDHOST/$feHost/" -e "s/FRONTENDPATH/$fePath/" < $configsrc > $configtarget
+sed -e "s/SUBSCRIBERID/$subID/" -e "s/LOCATIONID/$locID/" -e "s/GATEWAYNAME/$gwName/" -e "s/BACKENDHOST/$beHost/" -e "s/BACKENDPATH/$bePath/" -e "s/FRONTENDHOST/$feHost/" -e "s/FRONTENDPATH/$fePath/" < $configsrc > $configtarget
 
 chmod +x $configtarget
 
