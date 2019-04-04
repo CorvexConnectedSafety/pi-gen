fd5371d5c5ab39e6bda0d2b3100c7891382f9ad1
Shane McCarron <halindrome@gmail.com>
2019-04-04 09:07:56 -0500

    Added a separate autossh for tunneling.

--- stage2/04-corvex/userfiles/tunnel.sh
+++ stage2/04-corvex/userfiles/tunnel.sh
@@ -5,7 +5,8 @@ then
     . /var/www/html/scripts/tools/config.sh
     dport=`printf '11%03d' "$subscriber"`
     echo "Remote tunnel on port $dport"
-    autossh -f -N -p $port -o "StrictHostKeyChecking no" -i /home/corvex/tunnel_key -L 5667:dev.corvexconnected.com:5667 -L 3128:127.0.0.1:3128 -L 13306:127.0.0.1:3306 -L 8080:127.0.0.1:80 -R $dport:127.0.0.1:22 ccs@db1.corvexconnected.com 2>&1
+    autossh -f -N -p $port -o "StrictHostKeyChecking no" -i /home/corvex/tunnel_key -L 5667:dev.corvexconnected.com:5667 -L 3128:127.0.0.1:3128 -L 13306:127.0.0.1:3306 -L 8080:127.0.0.1:80 ccs@db1.corvexconnected.com 2>&1
+    autossh -f -N -p $port -o "StrictHostKeyChecking no" -i /home/corvex/tunnel_key -R $dport:127.0.0.1:22 ccs@db1.corvexconnected.com 2>&1
 fi
 
 
