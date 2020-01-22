0bbedc4bc82d13c130982c7212678032c409fb52
Shane McCarron <halindrome@gmail.com>
2020-01-21 21:47:26 -0600

    changes to migrate to docker

---
+++ stage2/04-corvex/userfiles/fetch-containers.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker pull registry.gitlab.com/corvexconnected/rest-api:arm-latest
+docker pull registry.gitlab.com/corvexconnected/webapp:arm-latest
+docker pull registry.gitlab.com/corvexconnected/mariadb:arm-latest
+docker pull memcached
